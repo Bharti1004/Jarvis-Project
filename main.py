9e1ae973298f6964cb1e88c55941518426ebe6cf
Anshika-singhal <anshikasinghal3014@gmail.com>
2025-03-17 23:29:39 +0530

    Jarvis

--- main.py
+++ main.py
@@ -2,7 +2,16 @@ import os
 
 import eel
 
+<<<<<<< HEAD
 eel.init("www")
 
+=======
+from engine.features import *
+
+eel.init("www")
+
+playAssistantSound()
+
+>>>>>>> 8c8b51f (Jarvis)
 os.system('start msedge.exe --app="http://localhost:8000/index.html"')
 eel.start('index.html', mode=None, host='localhost', block=True)
--- www/main.js
+++ www/main.js
@@ -9,4 +9,32 @@ $(document).ready(function () {
             effect:"bounceOut",
         },
     })
+<<<<<<< HEAD
+=======
+
+    //Siri Configuration
+    var siriWave = new SiriWave({
+        container: document.getElementById("siri-container"),
+        width: 840,
+        height: 200,
+        style: "ios9",
+        amplitude: "1",
+        speed: "0.30",
+        autostart: true
+      });
+
+      //Siri message animation
+      $('.siri-message').textillate({
+        loop: true,
+        sync: true,
+        in:{
+            effect:"fadeInUp",
+            sync: true
+        },
+        out:{
+            effect:"fadeOutUp",
+            sync: true
+        },
+    })
+>>>>>>> 8c8b51f (Jarvis)
 });
