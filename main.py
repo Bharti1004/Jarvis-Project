c42bd8727d7b749b534f20ac8657d26ebe3ea556
Anshika-singhal <anshikasinghal3014@gmail.com>
2025-03-20 22:05:45 +0530

    hotword integration

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ from engine.features import playAssistantSound
 # from engine.features import *
 from engine.command import *
 
-eel.init("www")
+def start():
+    eel.init("www")
 
-playAssistantSound()
-os.system('start msedge.exe --app="http://localhost:8000/index.html"')
-eel.start('index.html', mode=None, host='localhost', block=True)
+    playAssistantSound()
+    os.system('start msedge.exe --app="http://localhost:8000/index.html"')
+    eel.start('index.html', mode=None, host='localhost', block=True)
