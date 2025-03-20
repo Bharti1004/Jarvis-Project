56f65d8a4ad8bffbd6ec46a16bc8ccb6949690e4
Bharti1004 <bj163431@gmail.com>
2025-03-20 23:52:50 +0530

    update

--- www/main.js
+++ www/main.js
@@ -47,7 +47,8 @@ $(document).ready(function () {
     function doc_keyUp(e) {
         // this would test for whichever key is 40 (down arrow) and the ctrl key at the same time
 
-        if (e.key === 'j' && e.metaKey) {
+        if (e.key === 'j' && e.metaKey)  // here metakey is windowkey
+        {
             eel.playAssistantSound()
             $("#oval").attr("hidden", true);
             $("#SiriWave").attr("hidden", false);
