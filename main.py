0b781fab31333d08f2cf8f36f9a7226d2b66baa9
Anshika-singhal <anshikasinghal3014@gmail.com>
2025-04-25 16:13:41 +0530

    Face Recognition Backend

--- main.py
+++ main.py
@@ -7,10 +7,13 @@ eel.init("www")
 from engine.features import playAssistantSound
 # from engine.features import *
 from engine.command import *
+from engine.auth import recoganize
 
 def start():
     eel.init("www")
 
     playAssistantSound()
+    flag = recoganize.AuthenticateFace()
+    
     os.system('start msedge.exe --app="http://localhost:8000/index.html"')
     eel.start('index.html', mode=None, host='localhost', block=True)
